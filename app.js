7d01594209979bfb75f64ddefbfbde8b9dcfdc7f
Mary Cigliola <mary.cigliola@gmail.com>
2024-08-03 16:00:49 -0700

    Module 5 completed baseline

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
 var roomsRouter = require('./app_server/routes/rooms');
+var apiRouter = require('./app_api/routes/index');
+const { db } = require('./app_api/models/travlr');
+
+require('./app_api/models/db');
 
 var app = express();
 
@@ -29,6 +33,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
 app.use('/rooms', roomsRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
